bfb8811eed3d930d273eebffec6ac6770cda3d73
jeremie-codes <jrmmianda@gmail.com>
2026-06-23 18:46:38 +0100

    add table Resource

---
+++ app/Filament/Resources/Tables/Pages/CreateTable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Tables\Pages;
+
+use App\Filament\Resources\Tables\TableResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTable extends CreateRecord
+{
+    protected static string $resource = TableResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Filament/Resources/Tables/Tables/TablesTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Resources\Tables\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TablesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nom de la table')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->label('Ajouté le')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true)
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
